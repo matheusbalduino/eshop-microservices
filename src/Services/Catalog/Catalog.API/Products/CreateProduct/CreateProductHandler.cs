a97ae4d7db1377ef7780536153d148ed601985ca
matheusbalduino <matheus.balduin@gmail.com>
2025-04-19 13:15:46 +0200

    implementing mediator

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string name, List<string> Category, string description, string imageFile, decimal price) 
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Business logic goes here to create a product
+            throw new NotImplementedException();
+        }
+    }
+}
