c3b7e8639443de9a5b41ea19e768ebbe1c655a0a
matheusbalduino <matheus.balduino@mews.com>
2025-05-10 12:33:09 +0200

    docker compose created

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,9 @@
-﻿using BuildingBlocks;
-using Catalog.API.Models;
-using MediatR;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler 
+    internal class CreateProductCommandHandler(IDocumentSession session) 
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -23,8 +19,10 @@ namespace Catalog.API.Products.CreateProduct
             };
             
         // Save product entity
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
         // return CreateProductResult
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
         }
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -10,6 +10,10 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 builder.Services
     .AddGraphQLServer()
